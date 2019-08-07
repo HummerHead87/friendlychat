ba16554c13d0dece9f633052b77be8aeb652f0fd
Valeyev Rustam <snooks87@gmail.com>
2019-08-07 20:26:06 +0300

    start app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  new MaterialApp(
+    title: 'Friendlychat',
+    home: new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Friendlychat'),
+      ),
+    )
+  )
+);
+
